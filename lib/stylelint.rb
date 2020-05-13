423161178f48c18e82334bf9d9740f3038215017
Ubuntu <ubuntu@ip-172-31-35-111.us-east-2.compute.internal>
2020-05-13 10:59:29 +0000

    create rspec tests for the stylelint file

--- lib/stylelint.rb
+++ lib/stylelint.rb
@@ -16,61 +16,65 @@ class Stylelint < CssFiles
     end
     
     def px_measurement_not_allowed
+        msg = ""
+        msg_arr = []
         @page.each_selector do |one|
         decs = declarations(one)
         decs.each do |key, value|
         if(/px/.match("#{value}"))
-          puts "#{value} is not allowed. Please use rems,ems or percentage units.".colorize(:red)
-        else
-          puts "Test passed".colorize(:green)
+          msg = "#{value} is not allowed. Please use rems,ems or percentage units.".colorize(:red)
+          msg_arr << msg
         end
         end
         end
+        msg_arr.length > 0 ? msg_arr.each{|alert| alert} : "Test passed".colorize(:green)
     end
     
     def declaration_limit_10
+        msg = ""
+        msg_arr = []
         @page.each_selector do |one|
         decs = declarations(one)
         if decs.length > 10
-            puts "You cannot have more than 10 declarations in one selector".colorize(:red)
-        else
-            puts "Test passed for 10 declarations or less".colorize(:green)
+         msg = "You cannot have more than 10 declarations in one selector".colorize(:red)
+         msg_arr << msg
         end  
         end
+        msg_arr.length > 0 ? msg_arr.each{|alert| alert} : "Test passed for 10 declarations or less".colorize(:green)
     end
     
-    def id_limit_10
+    def id_limit_4
     no_of_ids = 0    
      @page.each_selector do |one|
      if one[0] == "#"
         no_of_ids += 1
      end
     end
-    if no_of_ids > 4
-        puts "You cannot use more that four ids in the css file.".colorize(:red)
-    else
-        puts "Test passed for four ids or less"
-    end
+    no_of_ids > 4 ? "You cannot use more that four ids in the css file.".colorize(:red) : "Test passed for four ids or less".colorize(:green)
     end
     
     def selector_names_lowercase
+        msg = ""
+        msg_arr = []
      @page.each_selector do |one|
      if(/[[:upper:]]/.match("#{one[0]}") || /[[:upper:]]/.match("#{one[1]}"))
-         puts "Do not use a capitalized class identifier, #{one}".colorize(:red)
-     else
-         puts "Test passed for selector names".colorize(:green)
+         msg = "Do not use a capitalized class identifier, #{one}".colorize(:red)
+         msg_arr << msg
      end
      end
+     msg_arr.length > 0 ? msg_arr.each{|alert| alert} : "Test passed for selector names".colorize(:green)
     end
     
     def selector_names_disallowed
+        msg = ""
+        msg_arr = []
     @page.each_selector do |one|
      if(one == ".body" || one == ".a" || one == ".html" || one == ".div")
-         puts "You are not supposed to use #{one} as a class/id identifier.".colorize(:red)
-     else
-         puts "Test passed for selector names chosen".colorize(:green)
+         msg = "You are not supposed to use #{one} as a class/id identifier.".colorize(:red)
+         msg_arr << msg
      end
     end
+    msg_arr.length > 0 ? msg_arr.each{|alert| alert} : "Test passed for selector names chosen".colorize(:green)
     end
     
     def validate
@@ -83,6 +87,12 @@ class Stylelint < CssFiles
     
 end
 
+
+
+test_file = Stylelint.new
+test_file.access_file('../../tester_file/spec_test.css')
+
+ puts test_file.selector_names_disallowed
 
 
 
